cf7a52ab90a0a95514f77c712e51a8c3b141fd6b
Alessia <auc74142@udelasalle.edu.mx>
2022-10-11 18:26:51 -0500

    usuario problema con imports

--- app/src/main/java/com/example/dressingmevol1/MainActivityUsuarios.kt
+++ app/src/main/java/com/example/dressingmevol1/MainActivityUsuarios.kt
@@ -35,7 +35,9 @@ class MainActivityUsuarios : AppCompatActivity() {
             val columnas = Usuario(
                 cursor.getInt(0),
                 cursor.getString(1),
-                cursor.getString(2)
+                cursor.getInt(2),
+                cursor.getString(3),
+                cursor.getString(4)
             )
             registros.add(columnas)
         }
